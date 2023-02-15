d6c94f88a95fabd8b617c931bd8ac4e3443cb52d
KolyaSamandasiuk <kolyasamandasyk@gmail.com>
2023-02-15 23:48:39 +0200

    add new automation test (KolyaSamandasiuk)

--- src/test/java/pages/SteamUaHomePageElements.java
+++ src/test/java/pages/SteamUaHomePageElements.java
@@ -10,7 +10,21 @@ public class SteamUaHomePageElements extends AbstractBasePage {
     }
     private final static String HEADER_LOGIN_BUTTON = "//a[text()='увійти']";
 
+    private final static String PULLWOWN_DESKTOP_CATEGORY = "(//a[text()='Категорії'])[1]";
+
+    private final static String BUTTON_ACTION_CATEGORY = "//a[contains(text(), 'Бойовики')]";
+
     public WebElement getOpenLoginWindow() {
         return waitUntilVisibilityOfElementLocated(HEADER_LOGIN_BUTTON);
     }
+
+    public WebElement getMoveCursorToCategory() {
+        return waitUntilVisibilityOfElementLocated(PULLWOWN_DESKTOP_CATEGORY);
+    }
+
+    public WebElement getClickCategoryToAction() {
+        return waitUntilVisibilityOfElementLocated(BUTTON_ACTION_CATEGORY);
+    }
+
 }
+
