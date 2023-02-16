adfbf14bc548cb556a81bf83bcbef96eaa592d8e
Maks Kondratenko <kzaraki1321@gmail.com>
2023-02-16 21:23:00 +0200

    add registration page elements and helper.add negative email registration test

---
+++ src/test/java/tests/SteamNegativeEmailRegistrationTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import base.AbstractBaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.SteamUaHomePageHelper;
+import pages.SteamUaRegistrationPageHelper;
+
+public class SteamNegativeEmailRegistrationTest extends AbstractBaseTest {
+    @Test
+    public void negativeEmailRegistrationTest() {
+        SteamUaHomePageHelper steamUaHomePageHelper = new SteamUaHomePageHelper(driver);
+        SteamUaRegistrationPageHelper steamUaRegistrationPageHelper=new SteamUaRegistrationPageHelper(driver);
+
+
+        openUrl("https://store.steampowered.com/");
+        steamUaHomePageHelper.openLoginWindow();
+        steamUaRegistrationPageHelper.clickOnJoinToSteamButton();
+        steamUaRegistrationPageHelper.writeValidEmail();
+        steamUaRegistrationPageHelper.clickOnContinueButton();
+
+
+        Assert.assertTrue(steamUaRegistrationPageHelper.getErrorDisplay().isDisplayed(), "Error display is visible");
+    }
+}
